ace1ac8a787c9fa7c765da25a183f295198a3484
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-09 14:55:08 +0200

    BinaryTreeZigzagLevelOrderTraversal

---
+++ algos/src/treesandgraphs/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,57 @@
+package treesandgraphs;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean rightSide = true;
+        while(!q.isEmpty()) {
+            int n = q.size();
+            LinkedList<Integer> subList = new LinkedList<>();
+            for(int i = 0; i < n; i++) {
+                TreeNode currentNode = q.poll();
+                if(currentNode.left != null) {
+                    q.add(currentNode.left);
+                }
+                if(currentNode.right != null) {
+                    q.add(currentNode.right);
+                }
+                if(rightSide) {
+                    subList.add(currentNode.val);
+                } else {
+                    subList.addFirst(currentNode.val);
+                }
+            }
+            rightSide = !rightSide;
+            list.add(subList);
+        }
+        return list;
+    }
+
+
+}
