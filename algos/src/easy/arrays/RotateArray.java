cfd291f5f392feece4c4d64b0dc08bc21b2251ea
AlexandreBessard <alexandre.bessard@gmail.com>
2021-10-07 12:08:36 +0200

    BestTimeToBuyAndSellStockII and RotateArray

---
+++ algos/src/easy/arrays/RotateArray.java
@@ -0,0 +1,38 @@
+package easy.arrays;
+
+import java.util.Arrays;
+
+//Given an array, rotate the array to the right by k steps, where k is non-negative.
+public class RotateArray {
+
+    /*
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+     */
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3   ;
+        //System.out.println(Arrays.toString(rotate(nums, k)));
+        rotate(nums, k);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        // speed up the rotation
+        k %= nums.length;
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
+        }
+    }
+
+
+}
